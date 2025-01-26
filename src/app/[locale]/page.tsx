b15cdcd262acf022114580f6ebb8f0f67bf564f4
LIU CHIAWEI <doublecheap@gmail.com>
2025-01-26 12:45:38 +0900

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,29 +6,38 @@ import { data } from "@/data/data";
 import ScrollDown from "@/components/scroll_down";
 import { useTranslations } from "next-intl";
 
+type ConsoleKey =
+  | "Famicom"
+  | "SuperFamicom"
+  | "Nintendo64"
+  | "GameCube"
+  | "Wii"
+  | "WiiU"
+  | "Switch"
+  | "Switch2";
+
 export default function Home() {
-  const t = useTranslations("Home");
+  const t = useTranslations("");
   return (
-    <div className="w-full">
+    <div>
       <HeaderLines
-        title="任天堂"
-        subtitle="クロニクル"
-        description="The Evolution of Nintendo"
+        title={t("Home.title")}
+        subtitle={t("Home.subtitle")}
+        description={t("Home.description")}
       />
       <div
         className="h-full md:h-screen max-w-7xl mx-auto py-20 flex flex-col justify-center items-center px-4 relative"
       >
         <h2 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4 text-black dark:text-white max-w-4xl">
-          任天堂の軌跡
+          {t("Home.intro_title")}
         </h2>
         <h3 className="text-base md:text-2xl mb-8 max-w-4xl">
-          8ビットから次世代へ
+          {t("Home.intro_subtitle")}
         </h3>
         <p className="text-neutral-600 dark:text-neutral-500 text-sm md:text-base max-w-sm text-justify">
-          任天堂が2025年に「Nintendo Switch 2」を発表しますね！ <br />
-          このサイトでは、初代ファミリーコンピュータから最新機種まで、任天堂のゲーム機がどのように進化し、革新を続けてきたのかを詳しく紹介しています。各ハードの特徴や発売当時のエピソード、そして名作ゲームの数々まで、たっぷりとお届け！
-          <br />
-          任天堂ファンはもちろん、ゲームの歴史に興味がある方にも楽しんでいただける情報満載のサイトです。ぜひ、懐かしの名機から最新ハードまで、一緒に任天堂の軌跡を振り返ってみませんか？
+          {t("Home.intro_description.0")}<br />
+          {t("Home.intro_description.1")}<br />
+          {t("Home.intro_description.2")}<br />
         </p>
         <ScrollDown />
       </div>
